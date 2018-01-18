922b68291689cffc7346dea5248516591d5371ca
Jake Betcher <jakebetcher@gmail.com>
2018-01-18 11:16:46 -0800

    add integration tests for GET and POST

---
+++ test/test-posts.js
@@ -0,0 +1,50 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const {app, runServer, closeServer} =require('../server');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Blog Posts', function() {
+
+  before(function() {
+    return runServer();
+  });
+
+  after(function() {
+    return closeServer;
+  });
+
+  it('should list items on GET', function() {
+    return chai.request(app)
+    .get('/blog-posts')
+    .then(function(res) {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('array');
+      const expectedKeys = ['title', 'content', 'author', 'publishDate'];
+      res.body.forEach(function(item) {
+        expect(item).to.be.a('object');
+        expect(item).to.include.keys(expectedKeys);
+      });
+    });
+  });
+
+  it('should add an item on POST', function() {
+    const newItem = {title: 'Python rocks', content: 'Python is the best language ever', author: 'John Jefferson'};
+    return chai.request(app)
+    .post('/blog-posts')
+    .send(newItem)
+    .then(function(res) {
+      expect(res).to.have.status(201);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('object');
+      expect(res.body).to.include.keys('title', 'content', 'author', 'publishDate');
+      expect(res.body.id).to.not.equal(null);
+      expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id, publishDate: res.body.publishDate}));
+
+    });
+  });
+
+
+});
